7530036886bed028762f42b1a4f50806e0410e4b
biancapacini <38740386+biancapacini@users.noreply.github.com>
2019-02-11 00:46:58 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   // You can add your own open weather map token here (https://home.openweathermap.org/api_keys)
   OPENWEATHER_TOKEN: process.env.OPENWEATHER_TOKEN || '',
   // You can add your own tmdb token here (https://www.themoviedb.org/)
-  MOVIEDB_TOKEN: process.env.MOVIEDB_TOKEN || '',
+  MOVIEDB_TOKEN: process.env.MOVIEDB_TOKEN || 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYTc4ZjI4NDg4YjY5YzZjYmQ0NDZiNWUxMTczMTFkYiIsInN1YiI6IjVjNjBkN2IxOTI1MTQxMmZiNGY5ZTc2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.U6_-EE2YMAeeaAmH6mjoZQJyg33e_kvNvq1cWzt5evM',
   PORT: process.env.PORT || 5000,
 };
